c475c69a97d82045fe4e646b40be1107bf735e4c
CuongDo203 <cdo85745@gmail.com>
2025-04-12 16:55:01 +0700

    Thêm giao diện add_plant_dialog cho trang my plant

--- app/src/main/java/com/example/myplantcare/fragments/MyPlantFragment.java
+++ app/src/main/java/com/example/myplantcare/fragments/MyPlantFragment.java
@@ -13,6 +13,7 @@ import android.view.ViewGroup;
 import com.example.myplantcare.R;
 import com.example.myplantcare.adapters.MyPlantAdapter;
 import com.example.myplantcare.models.MyPlantModel;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,14 +24,19 @@ public class MyPlantFragment extends Fragment {
     private RecyclerView recyclerView;
     private MyPlantAdapter adapter;
     private List<MyPlantModel> myPlantList;
+    private FloatingActionButton fabAddMyPlant;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_my_plant, container, false);
 
+        fabAddMyPlant = view.findViewById(R.id.fab_add_my_plant);
         recyclerView = view.findViewById(R.id.recycler_my_plant);
         recyclerView.setLayoutManager(new GridLayoutManager(getContext(), 2));
+
+        fabAddMyPlant.setOnClickListener(v -> showAddPlantDialog());
+
 
         myPlantList = new ArrayList<>();
         adapter = new MyPlantAdapter(myPlantList);
@@ -41,5 +47,10 @@ public class MyPlantFragment extends Fragment {
         adapter.notifyDataSetChanged();
 
         return view;
+    }
+
+    private void showAddPlantDialog() {
+        AddPlantDialogFragment dialogFragment = new AddPlantDialogFragment();
+        dialogFragment.show(getChildFragmentManager(), "AddPlantDialogTag");
     }
 }
